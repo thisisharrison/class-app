1b3a69585ec2d4d99c4805a990333dbfcd56e149
thisisharrison <harrison.lau@outlook.com>
2021-01-27 23:45:42 +0800

    Create register

---
+++ models/User.js
@@ -0,0 +1,37 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const UserSchema = new Schema ({
+    fname: {
+        type: String,
+        required: false
+    }, 
+    lname: {
+        type: String,
+        required: false
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    password: {
+        type: String,
+        required: true
+    },
+    bio: {
+        type: String,
+        required: false
+    },
+    isAdmin: {
+        type: Boolean,
+        default: false
+    },
+    // Will change this once ClassSchema and ScheduleSchema created 
+    saves: [String],
+    bookings: [String]
+}, {
+    timestamps: true
+})
+
+module.exports = User = mongoose.model('User', UserSchema);
--- routes/api/users.js
+++ routes/api/users.js
@@ -1,6 +1,35 @@
 const express = require("express");
+const bcrypt = require('bcryptjs');
 const router = express.Router();
 
+const User = require('../../models/User');
+
 router.get("/test", (req, res) => res.json({ msg: "This is the users route" }));
+
+router.post("/register", (req, res) => {
+    // Check if email exists
+    User.findOne({ email: req.body.email })
+        .then(user => {
+            if (user) {
+                // 400 error
+                return res.status(400).json({email: 'This email has already been taken.'})
+            } else {
+                const { fname, lname, email, password, bio, isAdmin } = req.body;
+                const newUser = new User ({
+                    fname, lname, email, password, bio, isAdmin
+                });
+                // Encrypt password and then save
+                bcrypt.genSalt(10, (err, salt) => {
+                    bcrypt.hash(newUser.password, salt, (err, hash) => {
+                        if (err) throw err; 
+                        newUser.password = hash;
+                        newUser.save()
+                            .then(user => res.json(user))
+                            .catch(err => console.log(err));
+                    });
+                });
+            }
+        });
+});
 
 module.exports = router;
