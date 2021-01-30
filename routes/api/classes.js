87ad57acfb7f710b8317717d1a34e41336f93691
thisisharrison <harrison.lau@outlook.com>
2021-01-30 14:48:30 +0800

    Class basic routes

--- routes/api/classes.js
+++ routes/api/classes.js
@@ -1,6 +1,9 @@
 const express = require('express');
 const router = express.Router();
-const Class = require('../../models/Class').Class;
+const mongoose = require('mongoose');
+const passport = require('passport');
+
+const Class = require('../../models/Class');
 
 router.get("/test", (req, res) => res.json({ msg: "This is the classes route" }));
 
@@ -9,5 +12,69 @@ router.get('/', (req, res) => {
     .then(classes => res.json(classes))
     .catch(err => res.status(404).json({ noclassesfound: 'No classes found.' }))
 });
+
+router.post('/', 
+  passport.authenticate('jwt', { session: false }),
+  (req, res) => {
+    const newClass = new Class ({
+      name: req.body.name,
+      description: req.body.description,
+      admin: req.user.id,
+      tags: req.body.tags,
+      languages: req.body.languages
+    })
+
+    newClass.save()
+      .then(_class => res.json(_class))
+  }
+);
+
+router.get('/:id', (req, res) => {
+  Class.findById(req.params.id)
+    .then(_class => res.json(_class))
+    .catch(err => 
+      res.status(404).json({ noclassfound: 'No class found with that ID' })
+    );
+});
+
+router.patch('/:id',
+  passport.authenticate('jwt', { session: false }),
+  (req, res) => {
+    // find by id, update, return modified class
+    Class.findByIdAndUpdate(req.params.id,
+      {$set: {
+        name: req.body.name,
+        description: req.body.description, 
+        tags: req.body.tags,
+        languages: req.body.languages
+      }},
+      {new: true},
+      (err, result) => {
+        if (err) {
+          res.status(404).json({ noclassfound: 'No class found with that ID' })
+        } else {
+          res.json(result)
+        }
+      }
+    )
+});
+
+router.delete('/:id', 
+  passport.authenticate('jwt', { session: false }),
+  (req, res) => {
+    // find by id, delete
+    Class.findByIdAndDelete(req.params.id, 
+      (err, result) => {
+        if (err) {
+          res.status(404).json({ noclassfound: 'No class found with that ID' })
+        } else {
+          res.json(result)
+        }
+      }  
+    )
+  }
+)
+
+
 
 module.exports = router;
