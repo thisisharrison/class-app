9f30a7be23c0a1d96037f59001a06203677a74a9
thisisharrison <harrison.lau@outlook.com>
2021-01-31 21:51:45 +0800

    Class API Util

---
+++ frontend/src/util/class/class_api_util.js
@@ -0,0 +1,21 @@
+import axios from 'axios';
+
+export const getClasses = () => (
+  axios.get('/api/classes')
+)
+
+export const postClass = data => (
+  axios.post('/api/classes', data)
+)
+
+export const showClass = id => (
+  axios.get(`/api/classes/${id}`)
+)
+
+export const patchClass = (id, data) => (
+  axios.patch(`/api/classes/${id}`, data)
+) 
+
+export const deleteClass = id => (
+  axios.delete(`/api/classes/${id}`)
+)
