0db77889e5d996452fa5e92cadb8586f8048ecba
thisisharrison <harrison.lau@outlook.com>
2021-02-06 13:27:58 +0800

    Profile Dashboard reducer

---
+++ frontend/src/reducers/dashboard_reducer.js
@@ -0,0 +1,39 @@
+import {
+  RECEIVE_BOOKINGS,
+  RECEIVE_BOOKING,
+  REMOVE_BOOKING,
+  RECEIVE_SAVES,
+  RECEIVE_SAVE,
+  REMOVE_SAVE
+} from '../actions/profile_actions';
+
+const initialState = { bookings: [], saves: [] };
+
+const dashboardReducer = (state = initialState, action) => {
+  let newState = Object.freeze({}, state);
+  
+  switch(action.type) {
+    case RECEIVE_BOOKINGS:
+      newState.bookings = action.bookings;
+      return newState;
+    case RECEIVE_BOOKING:
+      newState.bookings = action.bookings;
+      return newState;
+    case REMOVE_BOOKING:
+      newState.bookings = action.bookings;
+      return newState;
+    case RECEIVE_SAVES:
+      newState.saves = action.saves;
+      return newState;
+    case RECEIVE_SAVE:
+      newState.saves = action.saves;
+      return newState;
+    case REMOVE_SAVE:
+      newState.saves = action.saves;
+      return newState;
+    default:
+      return state;
+  }
+}
+
+export default dashboardReducer;
--- frontend/src/reducers/entities_reducer.js
+++ frontend/src/reducers/entities_reducer.js
@@ -1,6 +1,7 @@
 import { combineReducers } from "redux";
 import classes from './class/class_reducer'
 import classTimes from './classtime/classtime_reducer';
+import dashboard from './dashboard_reducer';
 
 export default combineReducers({
   classes,
