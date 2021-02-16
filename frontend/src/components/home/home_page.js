da6b02c13ea204e79ce423a5e4e17b8a0286f39b
thisisharrison <harrison.lau@outlook.com>
2021-02-16 20:03:25 +0800

    Homepage styled

--- frontend/src/components/home/home_page.js
+++ frontend/src/components/home/home_page.js
@@ -1,15 +1,57 @@
 import React, { Component } from 'react'
+import { Container, FormControl, FormHelperText, Grid, Paper, TextField, ThemeProvider } from '@material-ui/core'
+import GridList from '@material-ui/core/GridList';
+import GridListTile from '@material-ui/core/GridListTile';
+import styled from 'styled-components'
+import { SecondarySubmitInput } from '../session/session_style';
 
-export default class HomePage extends Component {
-  render() {
-    return (
-      <div>
-        <h1>lulukittens</h1>
-        <p>Coming Soon 🚧</p>
-        <footer>
-          Copyright &copy; 2021 H.L.
-        </footer>
-      </div>
-    )
-  }
+const HomePage = () => {
+  return (
+    <div>
+      <Container maxwidth='sm'>
+        <Grid container xs>
+          <Grid item xs={1}>
+            <div className='homepage-hero-links'>
+              <h1>lulukittens</h1>
+              <ul>
+                <li>Kitten's What's New</li>
+                <li>Cat's What's New</li>
+                <li>All Kittens</li>
+                <li>All Cats</li>
+                <li>All Accessories</li>
+              </ul>
+            </div>
+          </Grid>
+          <Grid item xs={11}>
+            <GridList>
+              <GridListTile key='Img1' rows='3'>
+                <img src='https://tn.fishki.net/26/upload/en/201305/16/10539/249ef8594bfc63ff717e6048dc454285.jpg' alt='Img1' />
+              </GridListTile>
+              <GridListTile key='Img2' rows='3'>
+                <img src='https://tn.fishki.net/26/upload/en/201305/16/10539/f6a0aefbf36aa3feee14cd6fc7013603.jpg' alt='Img2' />
+                <div className="homepage-hero-cta">
+                  <SecondarySubmitInput as="a" href="#/classes">EXPLORE CLASSES</SecondarySubmitInput>
+                </div>
+              </GridListTile>
+            </GridList>
+          </Grid>
+
+        </Grid>
+        <Grid container xs alignItems="center">
+          <Grid item xs={6}>
+            <h2>Sweat with Us Virtually on our Class App.</h2>
+          </Grid>
+          <Grid item xs={6}>
+            <p className="homepage-hero-paragraph">Our newest Online Class Experience platform connects you with our Kitten Ambassadors around the globe.</p>
+          </Grid>
+          <footer>
+            <p>Coming Soon 🚧</p>
+            <p>Copyright &copy; 2021 H.L.</p>
+          </footer>
+        </Grid>
+      </Container>
+    </div>
+  )
 }
+
+export default HomePage;
